0615b66212ec618960fd641c9a5ffad2ce88e944
dili <muhammaddilia99@gamil.com>
2026-04-05 22:16:57 +0700

    fix:validation zod

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,9 +1,9 @@
 import * as z from "zod";
 
 export const User = z.object({
-  id: z.uuid(),
+  id: z.string().uuid(),
   username: z.string().min(1, "Enter Your Username"),
-  email: z.email(),
+  email: z.string().email(),
   passwordHash: z
     .string()
     .min(8, "Password should at least 8 characters")
@@ -13,17 +13,17 @@ export const User = z.object({
     ),
 });
 export const Todos = z.object({
-  userId: z.uuid().min(1, "user id required"),
+  userId: z.string().uuid().min(1, "user id required"),
   title: z.string().min(1, "Title required"),
   descirption: z.string(),
   difficulty: z.enum(["easy", "medium", "hard"]),
   deadline: z.date().min(new Date(Date.now()), {
-    error: "Please select a date from today onwards. ",
+    message: "Please select a date from today onwards. ",
   }),
   status: z.enum(["todo", "in_progress", "done"]),
 });
 
 export const Token = z.object({
-  userId: z.uuid().min(1, "user id required"),
+  userId: z.string().uuid().min(1, "user id required"),
   token: z.string().min(1, "Token required"),
 });
